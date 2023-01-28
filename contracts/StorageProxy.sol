7aeb973c401158de6bdd6058c685e3c162416b6b
Aboudoc <aboutika.reda@gmail.com>
2023-01-28 11:47:25 +0100

    proxy

--- contracts/StorageProxy.sol
+++ contracts/StorageProxy.sol
@@ -4,18 +4,35 @@ pragma solidity ^0.8.17;
 import "./StorageSlot.sol";
 
 contract Proxy {
-
     fallback() external {
-        (bool success, ) = StorageSlot.getAddressSlot(keccak256("impl")).value.delegatecall(msg.data);
+        (bool success, ) = StorageSlot
+            .getAddressSlot(keccak256("impl"))
+            .value
+            .delegatecall(msg.data);
         require(success);
     }
 
     function changeImplementation(address _implementation) external {
         StorageSlot.getAddressSlot(keccak256("impl")).value = _implementation;
-    } 
-
+    }
 }
 
-contract logic1 {}
+contract logic1 {
+    uint x;
 
-contract logic2 {}
+    function changeX(uint _x) external {
+        x = _x;
+    }
+}
+
+contract logic2 {
+    uint x;
+
+    function changeX(uint _x) external {
+        x = _x;
+    }
+
+    function tripleX() external {
+        x *= 3;
+    }
+}
